9a8475e1222f3941b0a78bfbfe7c515ee919caee
fjmiguel0704 <fernandoj.miguel@iesnervion.es>
2022-11-17 13:44:20 +0100

    EjerciciosT3-FernandoJoséMiguel

--- EjerciciosT3-FernandoJoséMiguel/src/ejercicios/Ejercicio2.java
+++ EjerciciosT3-FernandoJoséMiguel/src/ejercicios/Ejercicio2.java
@@ -9,9 +9,6 @@ public class Ejercicio2 {
 		//Creamos la variable numero, que guardará un dato introducido por el teclado
 		int numero;
 		
-		//Esta contará el número de no primos
-		int contNoPrimo=0;
-		
 		//Contará el número de primos
 		int contPrimo=0;
 		
@@ -22,15 +19,18 @@ public class Ejercicio2 {
 		System.out.println("Introduce un número: ");
 		numero = read.nextInt();
 
-		//Creamos i y la incrementamos mientras sea menor a numero
-		for(int i = 2; i<numero; i++) {
+		//Creamos i, y la incrementamos mientras sea menor o igual a numero
+		for(int i = 2; i<=numero; i++) {
+			
+			//Incrementamos el contador de los primos en 1 cada vez que se ejecute el bucle
+			contPrimo++;
 			
 			//Creamos j y la incrementos mientras sea menor a i 
 			for(int j = 2; j<i; j++) {
 				
-				//Si i entre j da de resto 0, incrementos el contador de los no primos en 1
+				//Si i entre j da de resto 0, decrementamos el contador de primos en 1
 				if(i%j==0) {
-					contNoPrimo++;
+					contPrimo--;
 					
 					//Salimos del bucle
 					break;
@@ -38,11 +38,8 @@ public class Ejercicio2 {
 			}
 		}
 		
-		//El contador de primo va a ser igual al número menos el contador de no primos, menos 2
-		contPrimo=(numero-contNoPrimo)-2;
-		
-		//Mostramos el contador de primos por pantalla
-		System.out.println("Del 2 al " + (numero-1) + " hay " + contPrimo + " números primos");
+		//Mostramos el número de primos que se encuentra en el rango por pantalla
+		System.out.println("Del 2 al " + numero + " hay " + contPrimo + " números primos");
 		
 		//Cerramos el Scanner 
         read.close();
